6966e25d32239c5a8b976060d12bf12694d71720
Haidy08 <Haidi.sobhi.22@gmail.com>
2025-04-14 19:35:16 +0200

    UPDATE HAIDY

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { AuthComponent } from './auth-layout/auth/auth.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { BlankComponent } from './blank-layout/blank/blank.component';
-
-import { HomeComponent } from "./component/home/home.component";
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-
-  imports: [RouterOutlet,BlankComponent,AuthComponent,CommonModule,HomeComponent],
-
-
-
+  imports: [RouterOutlet,BlankComponent,AuthComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
